7d91a63a987d3abff343e806dac9fa03784817fc
Artuqu <percikartur@gmail.com>
2023-07-18 09:08:20 +0200

    Extract repeatable code to method

--- src/main/java/lambda/BlockLambdaDemo.java
+++ src/main/java/lambda/BlockLambdaDemo.java
@@ -2,27 +2,34 @@ package lambda;
 
 public class BlockLambdaDemo {
 
+
     public static void main(String[] args) {
         StringFunc reverse = (str) -> {
-            String result = "";
-            for (int i = str.length() - 1; i >= 0; i--) {
-                result += str.charAt(i);
-            }
-            return result;
+            return getStringStatic(str);
         };
         System.out.println("Lambda after reverse is: " + reverse.func("Lambda"));
         System.out.println("Expression after reverse is: " + reverse.func("Expression"));
 
 
         GenericInterface<String> reverse2 = (s -> {
-            String result = "";
-            for (int i = s.length() - 1; i >= 0; i--) {
-                result += s.charAt(i);
-            }
-            return result;
+            return getStringStatic(s);
         });
         System.out.println("Lambda after reverse is: " + reverse2.someFunc("Lambda"));
 
 
     }
+
+    public String getString(String str) {
+        String result = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            result += str.charAt(i);
+        }
+        return result;
+    }
+
+    public static String getStringStatic(String str){
+        BlockLambdaDemo blockLambdaDemo = new BlockLambdaDemo();
+        return blockLambdaDemo.getString(str);
+    }
+
 }
