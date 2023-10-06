38ab87b9d273a99f1b2e28215b630f03edadc63d
Artuqu <percikartur@gmail.com>
2023-10-06 12:24:08 +0200

    Currency

---
+++ src/main/java/util/CurrencyDemo.java
@@ -0,0 +1,18 @@
+package util;
+
+import java.util.Currency;
+import java.util.Locale;
+
+public class CurrencyDemo {
+
+
+    private static Currency instance;
+    private static String displayName;
+
+    public static void main(String[] args) {
+        instance = Currency.getInstance(Locale.US);
+        instance.getSymbol();
+        displayName = instance.getDisplayName();
+        System.out.println(displayName);
+    }
+}
