9f616d4fe7f4d3a697fa842a2a0ee11fe495805d
Artuqu <percikartur@gmail.com>
2023-11-14 13:51:21 +0100

    LocalDateTime formatting

--- src/main/java/date/DateDemo.java
+++ src/main/java/date/DateDemo.java
@@ -2,7 +2,10 @@ package date;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.Date;
 import java.util.Locale;
 
@@ -25,5 +28,14 @@ public class DateDemo {
 
         sdf = new SimpleDateFormat("dd MMM yyyy hh:mm:ss aa zzz");
         System.out.println(sdf.format(currentDate));
+
+        LocalDateTime ldt = LocalDateTime.now();
+        LocalDateTime localSet = LocalDateTime.of(2023, 3, 23, 20, 59);
+
+        LocalDateTime ldtMinus = ldt.minusMinutes(32);
+        System.out.println(ldt.format(DateTimeFormatter.ofPattern("MMMM d ',' yyyy hh':'mm a" )));
+        System.out.println(ldtMinus.format(DateTimeFormatter.ofPattern("MMMM d ',' yyyy HH':'mm a" )));
+        System.out.println(localSet.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)));
+
     }
 }
